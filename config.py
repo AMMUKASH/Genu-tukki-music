a32f95d912673c920805c6231f6c310448f23c08
𝐆𝛆𝛈𝛊𝛐𝛖𝛅ㅤ🏴‍☠️||鬼𝗚⽊𝛊神𝛐龙𝛅 🏴‍☠️ <misssqn@gmail.com>
2026-06-17 00:38:27 +0530

    Add configuration settings for API and database

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 38138069
+API_HASH = "2ed313ebcc45cbcf65d1fc736ec71681"
+BOT_TOKEN = "8997247669:AAGCgGGp24DWjRd-7FQ60mat_gQwe2Bs-9Y"
+MONGO_URL = "mongodb+srv://misssqn_db_user:Nova01@cluster0.6xxsrwq.mongodb.net/?retryWrites=true&w=majority"
+LOG_GROUP = -1003947649552
+OWNER = "@CoderNova"
+SUPPORT = "https://t.me/Genu_Bot_Support/119"
+BOT_USERNAME = "@Tukki_Music_Bot"
